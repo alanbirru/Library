78691d78dec3023032ba0b601d68ef15f83f43c8
birru <alanb_99@hotmail.com>
2022-05-05 15:42:24 -0600

    adding the modal and functionality

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-alert("uwu");
+// Modal appear selectors
+const addBookBtn = document.querySelector(".add-book-btn");
+
+const newBookModal = document.querySelector(".newBook-modal");
+const closeModalBtn = document.querySelector(".close");
+const addBtn = document.querySelector(".add");
+
+let myLibrary = [];
+
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+function addBookToLibrary(title, author, pages) {
+  myLibrary.push(new Book(title, author, pages));
+}
+
+function closeModalFunc() {
+  newBookModal.style.display = "none";
+}
+
+addBookBtn.addEventListener("click", function () {
+  newBookModal.style.display = "flex";
+});
+
+closeModalBtn.addEventListener("click", function () {
+  closeModalFunc();
+});
+
+window.onclick = function (event) {
+  if (event.target == newBookModal) {
+    closeModalFunc();
+  }
+};
